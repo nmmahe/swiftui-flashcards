dc2263a1aebb94a627f8fecfbcc676a18fd4bd41
Nick <nicholasmahe@gmail.com>
2019-12-03 18:28:33 -0700

    Initial Commit

---
+++ Flash Cards 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flash Cards 1
+//
+//  Created by Nick Mahe on 12/3/19.
+//  Copyright © 2019 Nick Mahe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
